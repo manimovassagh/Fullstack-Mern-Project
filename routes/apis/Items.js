bbc1bc6eebac2324901eb6b7d022d84025cf72ef
Mani Movassagh Ghazani <mani.mgh@gmail.com>
2021-06-12 15:12:16 +0200

    done

--- routes/apis/Items.js
+++ routes/apis/Items.js
@@ -8,14 +8,34 @@ const Item = require('../../models/Item')
 
 
 //@route Get api/item
-//@desc Creat a post
+//@desc Get All Items
 //@access Public
 
-router.post('/', (req, res) => {
+router.get('/', () => (req, res) => {
+  Item.find()
+    .then(Items => res.json(Items))
+});
+
+
+//@route Post api/item
+//@desc post new Items
+//@access Public
+
+router.post('/', () => (req, res) => {
   const newItem = new Item({
     name: req.body.name
-  })
-  newItem.save().then(item => res.json(item))
+  });
+  newItem.save().then(item => res.json(item));
 });
-debugger
-module.exports = router;
+
+//@route Delete api/item
+//@desc delete Item
+//@access Public
+
+router.delete('/:id', () => (req, res) => {
+  Item.findById(req.params.id)
+    .then(item => item.remove().then(() => res.json({ success: true })))
+    .catch(err => res.status(404).json({ success: false }))
+})
+
+module.exports = router
