87a723117179ab5490c7d67b1243d3b0dba4ca16
Mani Movassagh <mani.mgh@gmail.com>
2021-06-12 16:32:59 +0200

    commit done

--- routes/apis/Items.js
+++ routes/apis/Items.js
@@ -1,5 +1,4 @@
 const express = require('express');
-
 const router = express.Router();
 
 // Item model
@@ -9,19 +8,21 @@ const Item = require('../../models/Item')
 //@desc Get All Items
 //@access Public
 
-router.get('/', () => (req, res) => {
+router.get('/', (req, res) => {
   Item.find()
-    .then(Items => res.json(Items))
+    .sort({ date: -1 })
+    .then(items => res.json(items));
 });
 
 //@route Post api/item
 //@desc post new Items
 //@access Public
 
-router.post('/', () => (req, res) => {
+router.post('/', (req, res) => {
   const newItem = new Item({
     name: req.body.name
   });
+
   newItem.save().then(item => res.json(item));
 });
 
@@ -29,10 +30,10 @@ router.post('/', () => (req, res) => {
 //@desc delete Item
 //@access Public
 
-router.delete('/:id', () => (req, res) => {
+router.delete('/:id', (req, res) => {
   Item.findById(req.params.id)
     .then(item => item.remove().then(() => res.json({ success: true })))
-    .catch(err => res.status(404).json({ success: false }))
-})
+    .catch(err => res.status(404).json({ success: false }));
+});
 
-module.exports = router
+module.exports = router;
