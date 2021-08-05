5adf493dd63b401ce845a63342151552ed8f73a2
Mani Movassagh <you@example.com>
2021-08-05 23:41:49 +0200

    1

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
+//Information about mongodb connection
 module.exports = {
   mongoURI: 'mongodb+srv://mani:let1234@cluster0.zv6w6.mongodb.net/shoppinglist?retryWrites=true&w=majority'
   //mongodb+srv:
